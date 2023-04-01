290cd5bffd3554c6a699bb18735189786cb79952
Petr Kaiser <petr.kaiser@tul.cz>
2023-04-01 18:28:47 +0200

    pridany testy banky

--- server/src/test/java/cz/tul/stin/server/model/BankTest.java
+++ server/src/test/java/cz/tul/stin/server/model/BankTest.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BankTest {
-/*
+
     @Test
     public void testDownloadExchangeRates() {
         Assertions.assertDoesNotThrow(Bank::downloadExchangeRates);
@@ -19,7 +19,7 @@ public class BankTest {
 
     @Test
     public void testGetExchanegRateDate() throws Exception {
-        Assertions.assertEquals(Bank.getExchanegRateDate(), Const.JKEY_CNB_DATE);
+        Assertions.assertEquals(Bank.getExchanegRateDate(), "31.03.2023");
     }
 
     @Test
@@ -50,6 +50,5 @@ public class BankTest {
         Assertions.assertNotNull(code);
         Assertions.assertEquals(code.length(), 4);
     }
-    
- */
+
 }
